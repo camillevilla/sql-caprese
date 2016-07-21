c407cee4f8fa22cbc802d7c31d1ee4c547c9de59
cvilla100@gmail.com <cvilla100@gmail.com>
2016-07-20 17:03:49 -0700

    Add method for viewing pomodoros by single date

--- sql-caprese.rb
+++ sql-caprese.rb
@@ -40,30 +40,53 @@ db.execute(create_table_tags)
     #some method that reads the tags and adds them to the tags table
   end
 
+  #GAH all this ugly repetitive code. Eventually refactor and separate pretty print into a separate method.
   #View all pomodoros
   def view_all_poms(db)
     poms = db.execute("SELECT * FROM pomodoros")
-    tags = db.execute("SELECT * FROM tags")
     poms.each do |pom|
       puts "#{pom['id']} | #{pom['pomDate']} | #{pom['pomTime']} | #{pom['tags']} | #{pom['description']}"
     end
   end
 
-  #filter pomodoros by
-    #date => date range
+  #filter pomodoros by  
+    #date
+    def poms_by_date(db,pomDate)
+      poms = db.execute("SELECT * FROM pomodoros WHERE pomDate=?", [pomDate])
+      poms.each do |pom|
+        puts "#{pom['id']} | #{pom['pomDate']} | #{pom['pomTime']} | #{pom['tags']} | #{pom['description']}"
+      end
+    end
+
+    #date range
+    def poms_by_date_range(db,date1,date2)
+      #BLARGH need some kind of date-parsing method to understand a string as a range
+    end
     #tag
       #need to write some method that splits up a list of tags
+
+  #explore individual pomodoro
+ 
 
   #LAAAAAATER stuff
     #Edit pomodoros
     #Delete pomodoros
 
 #Somewhere down here will be a user interface
+  #[v]iew range
+    #[d]ate
+    #[t]ag
+    #[i]d
+      #[n]ext
+      #[p]revious
+  #[a]dd
 
 #driver code for testing
 add_pomodoro(db,'2016-07-19','1:30 PM', 'python','automate the boring stuff')
 add_pomodoro(db,'2016-07-20','2:30 PM','ruby','Sandi Metz book')
 view_all_poms(db)
+poms_by_date(db,"2016-07-19")
+poms_by_id(db,3)
 db.execute("INSERT INTO tags(name) VALUES('python')")
 db.execute("INSERT INTO tags(name) VALUES('ruby')")
 
