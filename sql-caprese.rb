8291631fd78e61e47772c50fbb0a64daba0feb0f
cvilla100@gmail.com <cvilla100@gmail.com>
2016-07-24 13:00:12 -0700

    Add user interface for adding and viewing pomodoros

--- sql-caprese.rb
+++ sql-caprese.rb
@@ -10,7 +10,6 @@ db = SQLite3::Database.new("pomLog.db")
 db.results_as_hash = true
 
 # create tables (if they don't already exist)
-
   #pomodoros table
   create_table_pom = <<-SQL
   CREATE TABLE IF NOT EXISTS pomodoros(
@@ -33,7 +32,7 @@ db.results_as_hash = true
 db.execute(create_table_pom)
 db.execute(create_table_tags)
 
-#Users should be able to
+# === METHOD DEFINITIONS ===
   #Add pomodoros
   def add_pomodoro(db,pomDate,pomTime,tags,description)
     db.execute("INSERT INTO pomodoros (pomDate, pomTime, tags, description) VALUES (?,?,?,?)", [pomDate, pomTime, tags, description])
@@ -96,18 +95,42 @@ db.execute(create_table_tags)
     #Edit pomodoros
     #Delete pomodoros
 
-#Somewhere down here will be a user interface
-  #[v]iew range
-    #[d]ate
-    #[t]ag
-    #[i]d
-      #[n]ext
-      #[p]revious
-  #[a]dd
+# === USER INTERFACE ===
+puts "TOMATOES EVERYWHERE!"
+user_input = ''
+while user_input != 'q'
+  puts "\n[a]dd pomodoros | [v]iew pomodoros | [q]uit"
+  user_input = gets.chomp
+  #Add pomodoro
+  if user_input =="a"
+      puts "add"
+  #View pomodoros
+  elsif user_input == "v"
+      puts "[va] View all | [vd] View by date"
+      user_input = gets.chomp
+      #View all
+      if user_input =="va"
+        view_all_poms(db)
+      #by single date
+      elsif user_input =="vd"
+        puts "Enter date (YYYY-MM-DD): "
+        date = gets.chomp
+        poms_by_date(db,date)
+      #by ID
+      #by tag
+      end
+  end
+end
 
-#driver code for testing
+
+# === DRIVER / TESTING CODE ===
+#some test entries to populate the database
+=begin
 add_pomodoro(db,'2016-07-19','1:30 PM', 'python','automate the boring stuff')
 add_pomodoro(db,'2016-07-20','2:30 PM','ruby','Sandi Metz book')
+=end
+
+=begin
 view_all_poms(db)
 poms_by_date(db,"2016-07-19")
 #poms_by_id(db,3)
@@ -116,3 +139,4 @@ db.execute("INSERT INTO tags(name) VALUES('ruby')")
 
 p db.execute("SELECT * FROM pomodoros WHERE tags='nyet';") == []
 p parse_tags("Python, ruby ,JAVASCRIPT ")
+=end
